ee2aa5304caa6c37ef8c56f541366a12be49a676
Martin Todorov <carlspring@gmail.com>
2013-09-10 14:51:53 +0100

    Fixed the test case.

--- strongbox-authentication/strongbox-authentication-ldap/src/test/java/org/carlspring/strongbox/jaas/LDAPLoginModuleACNTest.java
+++ strongbox-authentication/strongbox-authentication-ldap/src/test/java/org/carlspring/strongbox/jaas/LDAPLoginModuleACNTest.java
@@ -6,6 +6,7 @@ import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 
 import org.junit.Test;
+import static junit.framework.Assert.fail;
 
 /**
  * @author mtodorov
@@ -13,91 +14,82 @@ import org.junit.Test;
 public class LDAPLoginModuleACNTest
 {
 
-    private static final String VALID_DN_STRING_1 = "cn=Strong Box,ou=people,dc=carlspring,dc=org";
+    private static final String VALID_USER_1 = "strongbox";
 
-    private static final String VALID_DN_STRING_2 = "cn=Steve Todorov,ou=people,dc=carlspring,dc=org";
+    private static final String VALID_USER_2 = "stevetodorov";
 
-    private static final String VALID_DN_STRING_3 = "cn=Martin Todorov,ou=people,dc=carlspring,dc=org";
+    private static final String VALID_USER_3 = "martintodorov";
 
-    private static final String INVALID_DN_STRING_1 = "cn=Strong Box1,ou=people,dc=carlspring,dc=org";
+    private static final String INVALID_USER = "strongboxx";
 
-    private static final String USER_PASSWORD_VALID = "password";
+    private static final String PASSWORD_VALID = "password";
 
 
     @Test
     public void testValidLogin()
             throws LoginException
     {
-        System.out.println("\n\nTesting login with valid credentials...\n");
-
         System.out.println("Testing login with valid credentials...");
 
-
         // if we return with no exception, authentication succeeded
-        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_DN_STRING_1, USER_PASSWORD_VALID));
+        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, PASSWORD_VALID));
         lc.login();
-
-        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_DN_STRING_2, USER_PASSWORD_VALID));
-        lc.login();
-
-        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_DN_STRING_3, USER_PASSWORD_VALID));
-        lc.login();
-
-        /*
-        User ldapuser1 = user.findUser(VALID_DN_STRING_1, USER_PASSWORD_VALID);
-        User ldapuser2 = user.findUser(VALID_DN_STRING_2, USER_PASSWORD_VALID);
-        User ldapuser3 = user.findUser(VALID_DN_STRING_3, USER_PASSWORD_VALID);
-
-        assertEquals("Invalid LDAP user was retrieved!", "strongbox", ldapuser1.getUsername());
-        assertEquals("Invalid LDAP user was retrieved!", "stevetodorov", ldapuser2.getUsername());
-        assertEquals("Invalid LDAP user was retrieved!", "martintodorov", ldapuser3.getUsername());
-        */
     }
 
-    //    @Test
-//    public void testCaching()
-//            throws LoginException
-//    {
-//        /*
-//        System.out.println("Testing login with cached credentials...");
-//
-//        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(USERNAME_VALID, PASSWORD_VALID));
-//        lc.login();
-//
-//        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler("testuser", PASSWORD_VALID));
-//        lc.login();
-//
-//        // Try again
-//        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(USERNAME_VALID, PASSWORD_VALID));
-//        lc.login();
-//
-//        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler("testuser", PASSWORD_VALID));
-//        lc.login();
-//
-//        // if we return with no exception, authentication succeeded
-//        */
-//    }
-//
     /*
+    @Test
+    public void testCaching()
+            throws LoginException
+    {
+        System.out.println("Testing login with cached credentials...");
+
+        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, PASSWORD_VALID));
+        lc.login();
+
+        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_2, PASSWORD_VALID));
+        lc.login();
+
+        // Try again
+        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_3, PASSWORD_VALID));
+        lc.login();
+
+        lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, PASSWORD_VALID));
+        lc.login();
+
+        // if we return with no exception, authentication succeeded
+    }
+    */
+
     @Test
     public void testInvalidLogin()
             throws LoginException
     {
-        System.out.println("\n\nTesting login with invalid credentials...");
+        System.out.println("Testing login with invalid credentials...");
 
-        // attempt authentication
-        UsersDaoImpl user = new UsersDaoImpl();
+        LoginContext lc;
+
         try
         {
-            User ldapuser1 = user.findUser(INVALID_DN_STRING_1, USER_PASSWORD_VALID);
-            fail("The test should have failed when using invalid credentials!");
+            lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(INVALID_USER, PASSWORD_VALID));
+            lc.login();
+
+            fail("Logged in user with invalid login details!");
         }
-        catch (AuthenticationException e)
+        catch (LoginException e)
         {
-            //
+        }
+
+        try
+        {
+            lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, "password123"));
+            lc.login();
+
+            fail("Logged in user with invalid login details!");
+        }
+        catch (LoginException e)
+        {
         }
     }
-    */
 
 }
 
