1e5f1f420216464cd1df1119724c0c2e54e27a7d
Martin Todorov <carlspring@gmail.com>
2014-01-26 01:44:23 +0000

    Re-worked the LDAP test and made it use Spring.

--- strongbox-authentication/strongbox-authentication-ldap/src/test/java/org/carlspring/strongbox/jaas/LDAPLoginModuleACNTest.java
+++ strongbox-authentication/strongbox-authentication-ldap/src/test/java/org/carlspring/strongbox/jaas/LDAPLoginModuleACNTest.java
@@ -6,23 +6,26 @@ import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static junit.framework.Assert.fail;
 
 /**
  * @author mtodorov
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/META-INF/spring/strongbox-*-context.xml"})
 public class LDAPLoginModuleACNTest
 {
 
-    private static final String VALID_USER_1 = "strongbox";
-
-    private static final String VALID_USER_2 = "stevetodorov";
-
-    private static final String VALID_USER_3 = "martintodorov";
+    private static final String VALID_USER = "mtodorov";
 
     private static final String INVALID_USER = "strongboxx";
 
     private static final String PASSWORD_VALID = "password";
+
+    private static final String INVALID_PASSWORD = "password123";
 
 
     @Test
@@ -32,7 +35,7 @@ public class LDAPLoginModuleACNTest
         System.out.println("Testing login with valid credentials...");
 
         // if we return with no exception, authentication succeeded
-        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, PASSWORD_VALID));
+        LoginContext lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER, PASSWORD_VALID));
         lc.login();
     }
 
@@ -81,7 +84,7 @@ public class LDAPLoginModuleACNTest
 
         try
         {
-            lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER_1, "password123"));
+            lc = new LoginContext("strongbox", new AuthenticationCallbackHandler(VALID_USER, INVALID_PASSWORD));
             lc.login();
 
             fail("Logged in user with invalid login details!");
