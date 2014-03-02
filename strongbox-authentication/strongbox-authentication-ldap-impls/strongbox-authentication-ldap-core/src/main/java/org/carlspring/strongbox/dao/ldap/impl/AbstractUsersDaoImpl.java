8a1eec561d3a4fe2a49f572b81e13d612b8202c1
Martin Todorov <carlspring@gmail.com>
2014-03-02 03:53:15 +0000

    Removed an unused import.

--- strongbox-authentication/strongbox-authentication-ldap-impls/strongbox-authentication-ldap-core/src/main/java/org/carlspring/strongbox/dao/ldap/impl/AbstractUsersDaoImpl.java
+++ strongbox-authentication/strongbox-authentication-ldap-impls/strongbox-authentication-ldap-core/src/main/java/org/carlspring/strongbox/dao/ldap/impl/AbstractUsersDaoImpl.java
@@ -11,7 +11,6 @@ import java.util.Hashtable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
