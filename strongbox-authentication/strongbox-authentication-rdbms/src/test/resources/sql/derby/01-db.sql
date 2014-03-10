997ec5ae7295971713babf46685774d79e3d6621
Martin Todorov <carlspring@gmail.com>
2014-03-10 02:40:13 +0000

    #SB-142: Implement roles in the RDBMS module
    Re-worked the SQL script for creating the database as well as the test data.

--- strongbox-authentication/strongbox-authentication-rdbms/src/test/resources/sql/derby/01-db.sql
+++ strongbox-authentication/strongbox-authentication-rdbms/src/test/resources/sql/derby/01-db.sql
@@ -5,34 +5,37 @@ CREATE TABLE users (
        PASSWORD             VARCHAR(64)       NOT NULL,
        FULL_NAME            VARCHAR(64)       NOT NULL,
        EMAIL                VARCHAR(128)      NOT NULL,
-       PRIMARY KEY(USERNAME)
+       PRIMARY KEY (USERNAME)
 );
 
 CREATE TABLE access_roles (
-       ROLE_NAME            VARCHAR(64)       NOT NULL UNIQUE,
-       DESCRIPTION          VARCHAR(128)      NOT NULL
+       ROLE_NAME            VARCHAR(64)       NOT NULL,
+       DESCRIPTION          VARCHAR(128)      NOT NULL,
+       PRIMARY KEY (ROLE_NAME)
 );
 
 CREATE TABLE access_privileges (
        PRIVILEGE_NAME       VARCHAR(64)       NOT NULL,
        DESCRIPTION          VARCHAR(128)      NOT NULL,
-       PRIMARY KEY(PRIVILEGE_NAME)
+       PRIMARY KEY (PRIVILEGE_NAME)
 );
 
 CREATE TABLE role_mappings (
+       MAPPING_ID           INTEGER           NOT NULL GENERATED ALWAYS AS IDENTITY (start with 1, increment by 1),
        ROLE_NAME            VARCHAR(64)       NOT NULL,
-       PRIVILEGE_NAME       VARCHAR(64)       NOT NULL,
-       NESTED_ROLE_NAME     VARCHAR(64)       NOT NULL,
+       NESTED_ROLE_NAME     VARCHAR(64)       DEFAULT NULL,
+       PRIVILEGE_NAME       VARCHAR(64)       DEFAULT NULL,
+       PRIMARY KEY (MAPPING_ID),
+       CONSTRAINT fk_access_roles_rm_1 FOREIGN KEY (ROLE_NAME) REFERENCES access_roles (ROLE_NAME),
        CONSTRAINT fk_access_privileges FOREIGN KEY (PRIVILEGE_NAME) REFERENCES access_privileges(PRIVILEGE_NAME),
-       CONSTRAINT fk_access_roles_rm_1 FOREIGN KEY (ROLE_NAME) REFERENCES access_roles(ROLE_NAME),
        CONSTRAINT fk_access_roles_rm_2 FOREIGN KEY (NESTED_ROLE_NAME) REFERENCES access_roles(ROLE_NAME)
 );
 
 CREATE TABLE user_roles (
        USERNAME             VARCHAR(32)       NOT NULL,
        ROLE_NAME            VARCHAR(64)       NOT NULL,
-       PRIMARY KEY(USERNAME, ROLE_NAME),
+       PRIMARY KEY (USERNAME, ROLE_NAME),
        CONSTRAINT fk_users FOREIGN KEY (USERNAME) REFERENCES users(USERNAME),
-       CONSTRAINT fk_access_roles_us_1 FOREIGN KEY (ROLE_NAME) REFERENCES access_roles(ROLE_NAME)
+       CONSTRAINT fk_access_roles_us_1 FOREIGN KEY (ROLE_NAME) REFERENCES access_roles (ROLE_NAME)
 );
 
--- strongbox-authentication/strongbox-authentication-rdbms/src/test/resources/sql/derby/02-ddl.sql
+++ strongbox-authentication/strongbox-authentication-rdbms/src/test/resources/sql/derby/02-ddl.sql
@@ -8,12 +8,32 @@ INSERT INTO users (USERNAME, PASSWORD, FULL_NAME, EMAIL)
            VALUES ('testuser', '5f4dcc3b5aa765d61d8327deb882cf99', 'Testuser', 'testuser@carlspring.org');
            -- VALUES ('testuser', 'MD5:5f4dcc3b5aa765d61d8327deb882cf99');
 
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Administer', 'Administer');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Deploy', 'Deploy artifacts');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Download', 'Download artifacts');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Update', 'Update artifacts');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Delete', 'Delete artifacts');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Login', 'Login privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Reindex', 'Reindex privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Rebuild Maven Metadata', 'Rebuild Maven Metadata privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('Clear repository cache', 'Clear repository cache privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: Basic rights', 'Basic UI rights');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: Search', 'Search privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: View RSS feeds', 'View RSS feeds privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: Config files', 'View config files privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: Logs', 'View logs privilege');
+INSERT INTO access_privileges (PRIVILEGE_NAME, DESCRIPTION) VALUES ('UI: Logging', 'Manage and view logging privilege');
+
 INSERT INTO access_roles (role_name, description) VALUES ('ADMINISTRATOR', 'Administrative user role');
 INSERT INTO access_roles (role_name, description) VALUES ('USER', 'Regular user role');
-INSERT INTO access_roles (role_name, description) VALUES ('GET', 'Request artifacts');
-INSERT INTO access_roles (role_name, description) VALUES ('VIEW', 'Request artifacts');
-INSERT INTO access_roles (role_name, description) VALUES ('PUT', 'Upload artifacts');
-INSERT INTO access_roles (role_name, description) VALUES ('DELETE', 'Delete artifacts');
+INSERT INTO access_roles (role_name, description) VALUES ('UI: Basic rights', 'Basic UI rights.');
+INSERT INTO access_roles (role_name, description) VALUES ('Deployment', 'Deployment rights.');
+
+INSERT INTO role_mappings (ROLE_NAME, PRIVILEGE_NAME)   VALUES ('UI: Basic rights', 'Login');
+INSERT INTO role_mappings (ROLE_NAME, PRIVILEGE_NAME)   VALUES ('UI: Basic rights', 'UI: Search');
+INSERT INTO role_mappings (ROLE_NAME, PRIVILEGE_NAME)   VALUES ('UI: Basic rights', 'UI: View RSS feeds');
+INSERT INTO role_mappings (ROLE_NAME, PRIVILEGE_NAME)   VALUES ('Deployment', 'Deploy');
+INSERT INTO role_mappings (ROLE_NAME, NESTED_ROLE_NAME) VALUES ('Deployment', 'UI: Basic rights');
 
 -- Admin user
 INSERT INTO user_roles VALUES ('admin', 'ADMINISTRATOR');
